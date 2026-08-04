37c80832ea499ac64d0639400f217f9ae6809f3b
NgKhang302 <khannnng302@gmail.com>
2026-08-04 18:25:21 +0700

    Delete java/baitap directory

--- java/baitap/src/main/java/com/khang/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.khang.repository;
-// truy cập database cho category
-import com.khang.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository; //cấp full CRUD
-
-public interface CategoryRepository extends JpaRepository<Category,Integer> {
-}
